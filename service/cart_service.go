7dac29f2175d0e1b32ff365ae791c95010c2b8e8
Sanuka <sanuka.20232509@iit.ac.lk>
2025-10-28 14:26:03 +0530

    implemented the cart service

--- service/cart_service.go
+++ service/cart_service.go
@@ -22,7 +22,7 @@ func NewDatasetServiceImpl(CartRepository repository.CartRepository, ProductRepo
 	}, err
 }
 
-func (s *CartServiceImpl) GetUserCart(userId uint) (*model.Cart, error) {
+func (s *CartServiceImpl) GetUserCart(userId string) (*model.Cart, error) {
 	return s.CartRepository.GetUserCart(userId)
 }
 
@@ -61,6 +61,6 @@ func (s *CartServiceImpl) AddTOCart(userID uint, cartID uint, productID uint, qu
 	return nil
 }
 
-func (s *CartServiceImpl) ClearCart(userId uint) error {
+func (s *CartServiceImpl) ClearCart(userId string) error {
 	return s.CartRepository.ClearCart(userId)
 }
