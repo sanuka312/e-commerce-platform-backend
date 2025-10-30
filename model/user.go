70eaed2aeeeafdd5e4a63cf518ca7c0d215503c7
Sanuka <sanuka.20232509@iit.ac.lk>
2025-10-30 12:59:23 +0530

    Removed KeyCloakUserId and added userId as uint value

--- model/user.go
+++ model/user.go
@@ -1,7 +1,7 @@
 package model
 
 type User struct {
-	KeycloakUserId string `gorm:"size:100;not null" json:"keycloak_user_id"`
-	FullName       string `gorm:"size:250;not null" json:"full_name"`
-	UserEmail      string `gorm:"size:200;not null" json:"user_email"`
+	UserId    uint   `gorm:"size:100;not null" json:"keycloak_user_id"`
+	FullName  string `gorm:"size:250;not null" json:"full_name"`
+	UserEmail string `gorm:"size:200;not null" json:"user_email"`
 }
