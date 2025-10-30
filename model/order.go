9112fca4702f846a31a4eaed176e1d4e92c0eb11
Sanuka <sanuka.20232509@iit.ac.lk>
2025-10-30 12:58:18 +0530

    Updated order model file

--- model/order.go
+++ model/order.go
@@ -3,10 +3,10 @@ package model
 import "time"
 
 type Order struct {
-	OrderId   uint   `gorm:"PrimaryKey" json:"order_id"`
-	UserId    string `gorm:"not null" json:"user_id"`
-	ProductId uint   `gorm:"not null"`
-	PaymentId uint   `gorm:"not null"`
+	OrderId   uint `gorm:"PrimaryKey" json:"order_id"`
+	UserId    uint `gorm:"not null" json:"user_id"`
+	ProductId uint `gorm:"not null"`
+	PaymentId uint `gorm:"not null"`
 
 	ProductPrice float64 `json:"product_price"`
 	Quantity     uint    `gorm:"not null" json:"qty"`
@@ -19,7 +19,7 @@ type Order struct {
 	//Relationships
 	Product Product  `gorm:"foreignKey:ProductId" json:"product"`
 	Address *Address `gorm:"foreignKey:AddressId" json:"address"`
-	User    User     `gorm:"foreignKey:UserId;references:KeyCloakUserId" json:"user"`
+	User    User     `gorm:"foreignKey:UserId;references:UserId" json:"user"`
 	Payment Payment  `gorm:"foreignKey:PaymentId" json:"payment"`
 	Price   Product  `gorm:"foreignKey:ProductPrice" json:"price"`
 }
