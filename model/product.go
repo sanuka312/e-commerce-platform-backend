bc34af057d741a3993abd3db91ff929d3063edbd
Sanuka <sanuka.20232509@iit.ac.lk>
2025-10-31 16:50:38 +0530

    updated product model file

--- model/product.go
+++ model/product.go
@@ -4,7 +4,7 @@ type Product struct {
 	ProductID    uint    `gorm:"primaryKey" json:"product_id"`
 	ProductName  string  `gorm:"size:250; not null" json:"product_name"`
 	ProductPrice float64 `gorm:"not null" json:"price"`
-	ProductStock int     `gorm:"not null" json:"stock_qty"`
+	ProductStock int     `gorm:"not null" json:"product_stock"`
 
 	CategoryID   uint   `gorm:"not null" json:"category_id"`
 	CategoryName string `gorm:"size:100; not null" json:"category_name"`
