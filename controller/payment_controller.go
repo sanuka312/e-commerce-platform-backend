5ecd09db5ba1e6d79ecbceb30bcabbb7123f9b4f
Sanuka <sanuka.20232509@iit.ac.lk>
2025-11-04 10:11:17 +0530

    implemented payment_controller.go

--- controller/payment_controller.go
+++ controller/payment_controller.go
@@ -54,3 +54,58 @@ func (c *PaymentController) GetPaymentByOrderId(ctx *gin.Context) {
 	logger.ActInfo("Payments fetched successfully")
 	ctx.JSON(http.StatusOK, payment)
 }
+
+func (c *PaymentController) ProcessPayment(ctx *gin.Context) {
+	logger.ActInfo("Processing the payment")
+	idParam := ctx.Param("orderId")
+	if idParam == "" {
+		ctx.JSON(http.StatusBadRequest, data.ErrorResponse{
+			Error:            "Bad request",
+			ErrorDescription: "Missing OrderID parameter",
+		})
+		return
+	}
+
+	orderId, err := strconv.ParseUint(idParam, 10, 64)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, data.ErrorResponse{
+			Error:            "Bad Request",
+			ErrorDescription: "Invalid orderId",
+			Details:          err.Error(),
+		})
+		return
+	}
+
+	var req data.ProcessPaymentRequest
+
+	if err := ctx.ShouldBindJSON(&req); err != nil {
+		ctx.JSON(http.StatusInternalServerError, data.ErrorResponse{
+			Error:            "Internal Server Error",
+			ErrorDescription: "Invalid request body",
+			Details:          err.Error(),
+		})
+		return
+	}
+
+	if req.PaymentMethod == "" {
+		ctx.JSON(http.StatusBadRequest, data.ErrorResponse{
+			Error:            "Bad Request",
+			ErrorDescription: "Payment method is required",
+		})
+		return
+	}
+
+	payment, err := c.PaymentService.ProcessPayment(uint(orderId), req.PaymentMethod)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, data.ErrorResponse{
+			Error:            "Internal Server Error",
+			ErrorDescription: "Failed to process payment",
+			Details:          err.Error(),
+		})
+		return
+	}
+
+	logger.ActInfo("Payment processed successfully")
+	ctx.JSON(http.StatusOK, payment)
+
+}
