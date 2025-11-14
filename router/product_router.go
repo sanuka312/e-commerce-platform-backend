eb09db488ea93b085f76d63b4c7311ba9cda1967
Sanuka <sanuka.20232509@iit.ac.lk>
2025-11-14 17:10:35 +0530

    removed authentication middleware in product routes

--- router/product_router.go
+++ router/product_router.go
@@ -1,23 +1,24 @@
 package router
 
 import (
-	"shophub-backend/auth"
-
 	"github.com/gin-gonic/gin"
 )
 
 type ProductControllerInterface interface {
 	GetAllProducts(ctx *gin.Context)
 	GetProductById(ctx *gin.Context)
+	GetProductBySlug(ctx *gin.Context)
 }
 
 func RegisterProductRoutes(router *gin.Engine, controller ProductControllerInterface) {
-	authMiddleware := auth.AuthMiddleware()
-	productGroup := router.Group("/products", authMiddleware)
+
+	productGroup := router.Group("/products")
 	{
 
 		productGroup.GET("/", controller.GetAllProducts)
 		//Route for getting product by ID
 		productGroup.GET("/:id", controller.GetProductById)
+		//Route foe getting product by product slug
+		productGroup.GET("/slug/:productSlug", controller.GetProductBySlug)
 	}
 }
