8287b1b62f807756345a9402365723071e5e714f
Sanuka <sanuka.20232509@iit.ac.lk>
2025-11-14 17:09:03 +0530

    created the model for product images

--- model/product_image.go
+++ model/product_image.go
@@ -2,6 +2,9 @@ package model
 
 type ProductImage struct {
 	ImageID   uint   `gorm:"primaryKey" json:"image_id"`
-	ProductID uint   `gorm:"not null" json:"product_id"`
+	ProductID uint   `gorm:"not null;index" json:"product_id"`
 	ImageURL  string `gorm:"size:500; not null" json:"image_url"`
+
+	// Relationship
+	Product Product `gorm:"foreignKey:ProductID" json:"product"`
 }
