bce6e0da830ba593e87f3ed4e52fb1dade5288cd
Sanuka <sanuka.20232509@iit.ac.lk>
2025-11-14 17:09:18 +0530

    Updated product.go

--- model/product.go
+++ model/product.go
@@ -6,8 +6,11 @@ type Product struct {
 	ProductPrice float64 `gorm:"not null" json:"price"`
 	ProductStock int     `gorm:"not null" json:"product_stock"`
 	ProductSlug  string  `gorm:"size:250; unique; not null" json:"product_slug"`
-	CategoryID   uint    `gorm:"not null" json:"category_id"`
+	CategoryID   uint    `gorm:"not null;index" json:"category_id"`
 	CategoryName string  `gorm:"size:100; not null" json:"category_name"`
+	ImgUrlMain   string  `json:"image_url_main"`
 
+	// Relationships
+	Category      Category       `gorm:"foreignKey:CategoryID;references:CategoryID" json:"category"`
 	ProductImages []ProductImage `gorm:"foreignKey:ProductID" json:"product_images"`
 }
