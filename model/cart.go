63f772c01f53666a41d6a792e77602a18168df64
Sanuka <sanuka.20232509@iit.ac.lk>
2025-12-09 17:34:58 +0530

    updated cart models

--- model/cart.go
+++ model/cart.go
@@ -1,25 +1,23 @@
 package model
 
 type Cart struct {
-	CartID uint `gorm:"primaryKey" json:"cart_id"`
-	UserID uint `gorm:"not null" json:"user_id"` // Internal numeric user_id
+	CartID         uint   `gorm:"primaryKey" json:"cart_id"`
+	KeycloakUserID string `gorm:"not null;index" json:"keycloak_user_id"`
 
 	// Relationships
-	User  User       `gorm:"foreignKey:UserID;references:UserId" json:"user"`
+
 	Items []CartItem `gorm:"foreignKey:CartID" json:"cart_items"`
 }
 
 type CartItem struct {
-	ID          uint    `json:"id"`
-	CartID      uint    `gorm:"not null"`
-	ProductID   uint    `json:"product_id"`
-	ProductName string  `json:"product_name"`
-	UnitPrice   float64 `json:"unit_price"`
-	Quantity    int     `json:"quantity"`
-	TotalPrice  float64 `json:"total_price"`
-	IsSelected  bool    `json:"is_selected"`
+	ID        uint `gorm:"primaryKey" json:"id"`
+	CartID    uint `gorm:"not null"`
+	ProductID uint `json:"product_id"`
 
-	//Foreign Key to Cart ID
-	Cart    Cart    `gorm:"foreignKey:CartID" json:"cart"`
+	UnitPrice  float64 `json:"unit_price"`
+	Quantity   int     `json:"quantity"`
+	TotalPrice float64 `json:"total_price"`
+	IsSelected bool    `json:"is_selected"`
+
 	Product Product `gorm:"foreignKey:ProductID" json:"product"`
 }
