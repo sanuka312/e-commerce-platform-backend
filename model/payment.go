9171c29b71ca27301b09f5cfcc620ad577b3ace3
Sanuka <sanuka.20232509@iit.ac.lk>
2025-12-09 17:36:26 +0530

    updated commits to the model files

--- model/payment.go
+++ model/payment.go
@@ -1,13 +1,10 @@
 package model
 
 type Payment struct {
-	PaymentId     uint    `gorm:"PrimaryKey" json:"payment_id"`
-	OrderId       uint    `gorm:"not null" json:"order_id"`
-	UserId        uint    `gorm:"not null" json:"user_id"` // Internal numeric user_id
-	PaymentMethod string  `gorm:"size:100;not null" json:"payment_method"`
-	PaymentAmount float64 `gorm:"type:decimal(10,2);not null" json:"payment_amount"`
-	Status        string  `gorm:"size:50;not null;default:'pending'" json:"status"`
-
-	//Relationships
-	User User `gorm:"foreignKey:UserId;references:UserId" json:"user"`
+	PaymentId      uint    `gorm:"PrimaryKey" json:"payment_id"`
+	OrderId        *uint   `gorm:"index" json:"order_id"`
+	KeycloakUserID string  `gorm:"not null;index" json:"keycloak_user_id"`
+	PaymentMethod  string  `gorm:"size:100;not null" json:"payment_method"`
+	PaymentAmount  float64 `gorm:"type:decimal(10,2);not null" json:"payment_amount"`
+	Status         string  `gorm:"size:50;not null;default:'pending'" json:"status"`
 }
--- model/product.go
+++ model/product.go
@@ -3,12 +3,11 @@ package model
 type Product struct {
 	ProductID    uint    `gorm:"primaryKey" json:"product_id"`
 	ProductName  string  `gorm:"size:250; not null" json:"product_name"`
-	ProductPrice float64 `gorm:"not null" json:"price"`
+	ProductPrice float64 `gorm:"not null" json:"product_price"`
 	ProductStock int     `gorm:"not null" json:"product_stock"`
 	ProductSlug  string  `gorm:"size:250; unique; not null" json:"product_slug"`
 	CategoryID   uint    `gorm:"not null;index" json:"category_id"`
-	CategoryName string  `gorm:"size:100; not null" json:"category_name"`
-	ImgUrlMain   string  `json:"image_url_main"`
+	ImgUrlMain   string  `gorm:"column:image_url_main" json:"image_url_main"`
 
 	// Relationships
 	Category      Category       `gorm:"foreignKey:CategoryID;references:CategoryID" json:"category"`
