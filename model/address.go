5d81b5cb0a1d8fd0c5349c2053d70090e170dcc8
Sanuka <sanuka.20232509@iit.ac.lk>
2025-12-09 17:33:42 +0530

    Updated address model file

--- model/address.go
+++ model/address.go
@@ -1,14 +1,11 @@
 package model
 
 type Address struct {
-	AddressId  uint   `gorm:"primaryKey;autoIncrement" json:"address_id"`
-	UserId     uint   `gorm:"not null" json:"user_id"` // Internal numeric user_id
-	Line1      string `gorm:"size:200;not null" json:"line1"`
-	Line2      string `gorm:"size:200;not null" json:"line2"`
-	City       string `gorm:"size:100;not null" json:"city"`
-	PostalCode string `gorm:"size:100;not null" json:"postal_code"`
-	Country    string `gorm:"size:100;not null" json:"country"`
-
-	// Relationships
-	User User `gorm:"foreignKey:UserId;references:UserId" json:"user"`
+	AddressId      uint   `gorm:"primaryKey;autoIncrement" json:"address_id"`
+	KeycloakUserID string `gorm:"not null;index" json:"keycloak_user_id"`
+	Line1          string `gorm:"size:200;not null" json:"line1"`
+	Line2          string `gorm:"size:200;not null" json:"line2"`
+	City           string `gorm:"size:100;not null" json:"city"`
+	PostalCode     string `gorm:"size:100;not null" json:"postal_code"`
+	Country        string `gorm:"size:100;not null" json:"country"`
 }
