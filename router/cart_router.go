5e0dabeedcb431068d0c574c0f51826fb6053d15
Sanuka <sanuka.20232509@iit.ac.lk>
2025-12-10 10:32:59 +0530

    updated the routers

--- router/cart_router.go
+++ router/cart_router.go
@@ -11,15 +11,18 @@ type CartControllerInterface interface {
 	AddItemToCart(ctx *gin.Context)
 	ClearCart(ctx *gin.Context)
 	RemoveItemFromCart(ctx *gin.Context)
+	UpdateCartItemQuantity(ctx *gin.Context)
 }
 
 func RegisterCartRoutes(router *gin.Engine, controller CartControllerInterface) {
 	authMiddleware := auth.AuthMiddleware()
 	cartGroup := router.Group("/cart", authMiddleware)
 	{
+		cartGroup.GET("/", controller.GetUserCart)
 		cartGroup.GET("/:userId", controller.GetUserCart)
 		cartGroup.POST("/item", controller.AddItemToCart)
-		cartGroup.DELETE("/:userId/items", controller.ClearCart)
+		cartGroup.DELETE("/items", controller.ClearCart)
 		cartGroup.DELETE("/item/:itemId", controller.RemoveItemFromCart)
+		cartGroup.PATCH("/item/:itemId", controller.UpdateCartItemQuantity)
 	}
 }
